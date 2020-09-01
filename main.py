f5eb6b648670889627ecdb9b3e502e735a6c449e
rain1275 <kirakira7573@gmail.com>
2020-09-01 21:18:52 +0900

    タブ数制限　エラーメッセージ表示

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ def serch(event):
     input_string = editBox.get()
     num = numCoutBox.get()
     keywordSerch = blogSerch.GoogleSerch()
-    keywordSerch.googleSarch(num, input_string)
+    if int(num) >= 5:
+        tkMessageBox.showerror("タブ数が多いです。", "検索するタブの数が多すぎます。1~5で入力してください。")
+    else:
+        keywordSerch.googleSarch(num, input_string)
 
 if __name__ == "__main__":
     root = tkinter.Tk()
@@ -27,11 +30,11 @@ if __name__ == "__main__":
     editBox = tkinter.Entry(width=30)
     editBox.place(x=150, y=70)
 
-    label2 = tkinter.Label(root, text="開くタブの数:", bg="#F5F5F5")
+    label2 = tkinter.Label(root, text="開くタブの数(1~5):", bg="#F5F5F5")
     label2.place(x=50, y=100)
 
     numCoutBox = tkinter.Entry(width=5)
-    numCoutBox.place(x=150, y=100)
+    numCoutBox.place(x=170, y=100)
 
     # ボタン配置
     button = tkinter.Button(root, text=u'検索', width=30)
